29554a308c6a0fbd2bf578a951fa09ea1f99ef7c
Harshal <harshalpm16@gmail.com>
2026-09-07 13:26:29 +0530

    Time: 7 ms (82.83%), Space: 48.4 MB (15%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,33 @@
 class Solution {
+    boolean find(int[] piles, int h,int mid){
+            int hours =0;
+            int n = piles.length;
+            for(int i=0;i<n;i++){
+                hours += (piles[i] + mid-1)/mid;
+                if(hours>h){
+                    return false;
+                }
+            }
+            return true;
+    }
     public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int s=1, e=-1, ans=-1;
 
-        int start = 1;
-        int end = 0;
-
-        // Maximum possible speed
-        for (int pile : piles) {
-            end = Math.max(end, pile);
+        for(int i=0; i<n;i++){
+            e = Math.max(piles[i],e);
         }
 
-        while (start < end) {
-
-            int mid = start + (end - start) / 2;
-
-            long hours = 0;
-
-            for (int pile : piles) {
-                hours += (pile + mid - 1) / mid;
-            }
-
-            if (hours <= h) {
-                // mid is possible, try a smaller speed
-                end = mid;
-            } else {
-                // mid is too slow
-                start = mid + 1;
+        while (s<=e) {
+            int mid = s+ (e-s)/2;
+            boolean flag = find(piles,h,mid);
+            if(flag == true){
+                ans = mid;
+                e = mid-1;
+            }else{
+                s =mid+1;
             }
         }
-
-        return start;
+        return ans;
     }
 }
